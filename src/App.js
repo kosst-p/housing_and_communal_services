bc3e1328e93ef565deb542cdc66be287813a02d3
kosst-p <pirozhkovks@yandex.ru>
2023-07-29 23:48:16 +0300

    Init commit, configure webpack+react.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+    return (
+        <React.StrictMode>
+            <div>Header</div>
+            <div>Body</div>
+            <div>Footer</div>
+        </React.StrictMode>
+    )
+}
